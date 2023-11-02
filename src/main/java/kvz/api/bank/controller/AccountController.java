ef92a263d93cfb3334d2325b39cf4996a81d643d
Diego Gordon <dgordon@kvz.cl>
2023-11-02 14:44:10 -0500

    Se corrige palabra

--- src/main/java/kvz/api/bank/controller/AccountController.java
+++ src/main/java/kvz/api/bank/controller/AccountController.java
@@ -12,7 +12,7 @@ public class AccountController {
 
     @GetMapping("get")
     public Account getAccount() {
-        return new Account("Ahorross", "12001212452", "1");
+        return new Account("Ahorros", "12001212452", "1");
     }
 
 /*    @GetMapping("validate")
